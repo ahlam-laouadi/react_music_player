13ea35a2d2ce29602564b170f0464656752163ef
ahlam-laouadi <ahlamahlamitaa144@gmail.com>
2023-02-14 17:33:17 +0000

    first commit

--- src/songs.js
+++ src/songs.js
@@ -1,19 +1,19 @@
 export const songs = [
     {
       title: "Larzal Fatma",
-      artist: "Sanae Marahati",
+      artist: "Sanaa Marahati",
       img: "./images/img1.jpg",
       src: "./songs/laghzal_Fatma.mp3",
     },
     {
       title: "LALLA GHITA MOULATI",
-      artist: "Sanae Marahati",
+      artist: "Sanaa Marahati",
       img: "./images/img1.jpg",
       src: "./songs/LALLA_GHITA_MOULATI.mp3",
     },
     {
       title: "Manachi Moulou3a",
-      artist: "Sanae Marahati",
+      artist: "Sanaa Marahati",
       img: "./images/img1.jpg",
       src: "./songs/Manachi_Moulou3a.mp3",
     },
